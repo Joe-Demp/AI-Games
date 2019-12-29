9a7cd91c744b4485195f6c98d2f3305d82414e38
Joe-Demp <dempseyjoseph96@yahoo.com>
2019-12-29 23:43:20 +0000

    added an evaluate and minimax method

--- ttt_mini_max_player.rb
+++ ttt_mini_max_player.rb
@@ -4,5 +4,15 @@ require_relative 'player.rb'
 
 # Documentation about TTTMiniMaxPlayer
 class TTTMiniMaxPlayer < Player
+  # Calls a method that implements the MiniMax algorithm
+  # takes in the second row of the MiniMax tree
   def get_move(boards_and_moves); end
+
+  def minimax_get_move(boards, maximize=true); end
+
+  def evaluate(board)
+    return 0 unless board.finished?
+
+    winning_symbol == @symbol ? 1 : -1
+  end
 end
