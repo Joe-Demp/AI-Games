9e15ecf61cc6575096a28c7a4b2a480998c329da
Joe-Demp <dempseyjoseph96@yahoo.com>
2020-01-02 17:56:27 +0000

    Added 2 more tests to TTTMiniMaxPlayerTest

--- tests/ttt_mini_max_player_test.rb
+++ tests/ttt_mini_max_player_test.rb
@@ -11,10 +11,6 @@ class TTTMiniMaxPlayerTest < Test::Unit::TestCase
     @game_a = TicTacToe.new(@player_x, @player_o)
 
     TicTacToe.send(:attr_writer, :state)
-    @game_a.state =
-      [BoardGame::SPACE, BoardGame::SPACE, BoardGame::SPACE,
-       :x, :o, :x,
-       :x, :x, :o]
   end
 
   def teardown
@@ -22,11 +18,54 @@ class TTTMiniMaxPlayerTest < Test::Unit::TestCase
   end
 
   def test_get_move1
+    @game_a.state =
+      [BoardGame::SPACE, BoardGame::SPACE, BoardGame::SPACE,
+       :x, :o, :x,
+       :x, :x, :o]
+
     children = []
     @game_a.each_next_board_and_move(@player_x.symbol) do |board, move|
       children << [board, move]
     end
 
     assert_equal(Move.new(0, 0), @player_x.get_move(children))
+  end
+
+  def test_get_move2
+    @game_a.state =
+      [:x, BoardGame::SPACE, :x,
+       BoardGame::SPACE, :o, :o,
+       :x, BoardGame::SPACE, BoardGame::SPACE]
+
+    children = []
+    @game_a.each_next_board_and_move(@player_x.symbol) do |board, move|
+      children << [board, move]
+    end
+    assert_equal(Move.new(0, 1), @player_x.get_move(children))
+
+    children = []
+    @game_a.each_next_board_and_move(@player_o.symbol) do |board, move|
+      children << [board, move]
+    end
+    assert_equal(Move.new(1, 0), @player_o.get_move(children))
+  end
+
+  def test_get_move3
+    @game_a.state =
+      [BoardGame::SPACE, BoardGame::SPACE, :o,
+       :x, :o, :x,
+       BoardGame::SPACE, BoardGame::SPACE, BoardGame::SPACE]
+
+    children = []
+    @game_a.each_next_board_and_move(@player_x.symbol) do |board, move|
+      children << [board, move]
+    end
+    assert_equal(Move.new(2, 0), @player_x.get_move(children))
+
+    children = []
+    @game_a.each_next_board_and_move(@player_o.symbol) do |board, move|
+      children << [board, move]
+    end
+    assert_equal(Move.new(2, 0), @player_o.get_move(children))
   end
 end
